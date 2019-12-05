40a65f0b8029ad96c1605731cc46ed0d00242e2c
Andrei Osipenko <aosipenko@tacitknowledge.com>
2019-12-05 23:59:16 +0300

    Improved performance on 2%

--- src/main/java/org/ao/robopaint/norm/SpeedNormCalculator.java
+++ src/main/java/org/ao/robopaint/norm/SpeedNormCalculator.java
@@ -16,33 +16,49 @@ public class SpeedNormCalculator implements NormCalculator {
         }
 
 //        result += calculateNorm(lines[0].x1, lines[0].y1, lines[0].x2, lines[0].y2);
+        Line previousLine = lines[0];
+        boolean previousReverse = lineImage.reverse[0];
         for (int i = 1; i < lines.length; i++) {
-            Line previousLine = lines[i - 1];
-            boolean previousReverse = lineImage.reverse[i - 1];
             Line line = lines[i];
             boolean reverse = lineImage.reverse[i];
 
             result += calculateNorm(previousLine, previousReverse, line, reverse);
-        }
-        return result;
-    }
-
-    public double calculateNorm(Line previousLine, boolean previousReverse, Line line, boolean reverse){
-        double result = 0;
-
-        int previousLineEndX = previousReverse ? previousLine.x1 : previousLine.x2;
-        int previousLineEndY = previousReverse ? previousLine.y1 : previousLine.y2;
-        int lineStartX = reverse ? line.x2 : line.x1;
-        int lineStartY = reverse ? line.y2 : line.y1;
-
-        result += calculateNorm(previousLineEndX, previousLineEndY, lineStartX, lineStartY);
-        if (previousLineEndX != lineStartX || previousLineEndY != lineStartY){
-            result += WHITESPACE_TO_LINE_GAP;
+            previousLine = line;
+            previousReverse = reverse;
         }
         return result;
     }
 
     private static double calculateNorm(int x1, int y1, int x2, int y2){
         return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+    public double calculateNorm(Line previousLine, boolean previousReverse, Line line, boolean reverse){
+
+        final int previousLineEndX;
+        final int previousLineEndY;
+        if (previousReverse) {
+            previousLineEndX = previousLine.x1;
+            previousLineEndY = previousLine.y1;
+        }
+        else {
+            previousLineEndX = previousLine.x2;
+            previousLineEndY = previousLine.y2;
+        }
+        final int lineStartX;
+        final int lineStartY;
+        if (reverse) {
+            lineStartX = line.x2;
+            lineStartY = line.y2;
+        }
+        else {
+            lineStartX = line.x1;
+            lineStartY = line.y1;
+        }
+        double result = calculateNorm(previousLineEndX, previousLineEndY, lineStartX, lineStartY);
+        if (previousLineEndX != lineStartX || previousLineEndY != lineStartY){
+            result += WHITESPACE_TO_LINE_GAP;
+        }
+        return result;
     }
 }

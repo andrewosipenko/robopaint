4bcc1eac442818e20bfc99866522e4fb8413ef31
Andrei Osipenko <aosipenko@tacitknowledge.com>
2020-01-08 22:44:22 +0300

    Implemented indexed / regular image conversion

--- src/main/java/org/ao/robopaint/image/Point.java
+++ src/main/java/org/ao/robopaint/image/Point.java
@@ -1,5 +1,7 @@
 package org.ao.robopaint.image;
 
+import java.util.Objects;
+
 public class Point {
     public int x;
     public int y;
@@ -7,5 +9,18 @@ public class Point {
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }

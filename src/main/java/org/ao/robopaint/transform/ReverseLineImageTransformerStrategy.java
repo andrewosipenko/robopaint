429dcf3bbd264d9be0113b11f106547762b04c01
Andrei Osipenko <aosipenko@tacitknowledge.com>
2020-03-07 09:58:39 +0300

    Fixed reverse quality

--- src/main/java/org/ao/robopaint/transform/ReverseLineImageTransformerStrategy.java
+++ src/main/java/org/ao/robopaint/transform/ReverseLineImageTransformerStrategy.java
@@ -1,6 +1,6 @@
 package org.ao.robopaint.transform;
 
-import org.ao.robopaint.norm.NormCalculator;
+import org.ao.robopaint.image.Line;
 import org.ao.robopaint.norm.NormedLineImage;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -14,11 +14,24 @@ public class ReverseLineImageTransformerStrategy implements LineImageTransformer
 
     @Override
     public void transform(NormedLineImage source, NormedLineImage target) {
-        int area = ThreadLocalRandom.current().nextInt((int)(ratio * (source.lines.length - 2))) + 1;
-        int start = ThreadLocalRandom.current().nextInt(source.lines.length - area - 1);
+        int area = ThreadLocalRandom.current().nextInt((int)(ratio * (source.lines.length - 1)) + 1) + 1;
+        int start = ThreadLocalRandom.current().nextInt(source.lines.length - area);
 
         target.clone(source);
-        for(int i = start; i < start + area; i++) {
+        int end = start + area;
+        if(area > 1) {
+            for (int i = 0; i <= area / 2; i++) {
+                Line line = target.lines[start + i];
+                target.lines[start + i] = target.lines[end - i];
+                target.lines[end - i] = line;
+
+                boolean reverse = target.reverse[start + i];
+                target.reverse[start + i] = !target.reverse[end - i];
+                target.reverse[end - i] = reverse;
+            }
+        }
+        if(area % 2 == 1){
+            int i = start + area / 2 + 1;
             target.reverse[i] = !target.reverse[i];
         }
     }
